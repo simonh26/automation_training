bdb59e232c768a5e7faa7962a1f13d5a2b4a65cb
Natalia Mitrofanova <natalia@shopkeep.com>
2017-10-19 06:17:49 -0400

    Using xpath to find table or element

--- Natasha/ShopkeepBackOffice.rb
+++ Natasha/ShopkeepBackOffice.rb
@@ -16,26 +16,43 @@ driver.manage.timeouts.implicit_wait = 10
 @login.enter_password'111111111A'
 @login.click_login_button
 
-@navigate_sidebar = Navigate_sidebar.new driver
-@navigate_sidebar.expandParent_clickitemlist
-
- @create_item= CreateItem.new driver
- rand_number = rand(1..5000000000)
- rand_number_price = rand(1..100)
- @create_item.create_item rand_number,rand_number_price,'0',3
-
-@adjust_inventory= AdjustInventory.new driver
-@adjust_inventory.navigate_to_url  'https://www.getshopkeep.com/mobile/take-inventory'
-@adjust_inventory.enter_item_name  'Basic Item1'
-@adjust_inventory.adjust_quantity  '20'
-@adjust_inventory.click_add_count
-@adjust_inventory.check_message  '20 (+ 0 in kits)'
-
-@add_modifiers= AddModifiers.new driver
-@add_modifiers.navigate_to_url  'https://www.getshopkeep.com/stock_items/18211156/modifiers'
-@add_modifiers.add_single_choice_group 'Single Choice Group Test'
-@add_modifiers.add_single_choice_option 'Test1'
-@add_modifiers.save_modifiers
+# @navigate_sidebar = Navigate_sidebar.new driver
+# @navigate_sidebar.expandParent_clickitemlist
+#
+#  @create_item= CreateItem.new driver
+#  rand_number = rand(1..5000000000)
+#  rand_number_price = rand(1..100)
+#  @create_item.create_item rand_number,rand_number_price,'0',3
+#
+# @adjust_inventory= AdjustInventory.new driver
+# @adjust_inventory.navigate_to_url  'https://www.getshopkeep.com/mobile/take-inventory'
+# @adjust_inventory.enter_item_name  'Basic Item1'
+# @adjust_inventory.adjust_quantity  '20'
+# @adjust_inventory.click_add_count
+# @adjust_inventory.check_message  '20 (+ 0 in kits)'
+#
+# @add_modifiers= AddModifiers.new driver
+# @add_modifiers.navigate_to_url  'https://www.getshopkeep.com/stock_items/18211156/modifiers'
+# @add_modifiers.add_single_choice_group 'Single Choice Group Test'
+# @add_modifiers.add_single_choice_option 'Test1'
+# @add_modifiers.save_modifiers
 
 #test
 
+driver.navigate.to 'https://www.getshopkeep.com/stock_items'
+
+
+table = driver.find_element(xpath: "//table[@id='DataTables_Table_0']")
+
+text = table.find_element(xpath: "//table[@id='DataTables_Table_0']/tbody/tr[2]/td[@class=' inventory-items-datatable__name']").text
+
+driver.find_elements(xpath: "//table[@id='DataTables_Table_0']/tbody/tr/td[@class=' inventory-items-datatable__name']").each do |r|
+ puts r.text
+  if r.text == 'Basic Item2' then
+    r.click
+    break
+  end
+end
+
+
+test
